e4a6063edcf550fe7d754523d412296363cb820d
Nathaniel Mosco <natemosco@gmail.com>
2019-10-14 18:41:19 -0400

    initial setup both servers running; added browserRouter; 2 Routes in place; 1 Link added to each Movie

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-
+import Route from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/"></Route>
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom'
 const MovieList = props => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
@@ -14,14 +14,16 @@ const MovieList = props => {
           console.error('Server Error', error);
         });
     }
-    
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to="/movies/:id">
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
