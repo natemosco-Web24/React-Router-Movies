7f3bb1ee029d707d2fe8b78ffdb594f51b971b29
Nathaniel Mosco <natemosco@gmail.com>
2019-10-15 19:43:43 -0400

    adding movies completely functional

--- client/src/Movies/MovieDetails.js
+++ client/src/Movies/MovieDetails.js
@@ -1,26 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function MovieDetails(props) {
 
-    const { title, director, metascore, stars } = props.movie;
+    const { title, director, metascore, stars, id } = props.movie;
     return (
         <div className="save-wrapper">
-            <div className="movie-card">
-                <h2>{title}</h2>
-                <div className="movie-director">
-                    Director: <em>{director}</em>
-                </div>
-                <div className="movie-metascore">
-                    Metascore: <strong>{metascore}</strong>
-                </div>
-                <h3>Actors</h3>
-
-                {stars.map(star => (
-                    <div key={star} className="movie-star">
-                        {star}
+            <Link to={`/movies/${id}`}>
+                <div className="movie-card">
+                    <h2>{title}</h2>
+                    <div className="movie-director">
+                        Director: <em>{director}</em>
                     </div>
-                ))}
-            </div>
+                    <div className="movie-metascore">
+                        Metascore: <strong>{metascore}</strong>
+                    </div>
+                    <h3>Actors</h3>
+
+                    {stars.map(star => (
+                        <div key={star} className="movie-star">
+                            {star}
+                        </div>
+                    ))}
+                </div>
+            </Link>
             <div onClick={() => { props.saveMovie() }} className="save-button">Save</div>
         </div>
     );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import MovieDetails from "./MovieDetails";
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -22,9 +21,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <Link to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
-        </Link>
+        <MovieDetails key={movie.id} saveMovie={() => { props.addToSavedList(movie) }} movie={movie} />
       ))}
     </div>
   );
